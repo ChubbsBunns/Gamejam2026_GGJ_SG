7155b93d2a9ed3cb591d8630331cfb147e3d627d
unknown <troskproductions@gmail.com>
2026-01-30 11:38:56 +0800

    Test commit

---
+++ Assets/testtest.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class testtest : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        print("I am a test script");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

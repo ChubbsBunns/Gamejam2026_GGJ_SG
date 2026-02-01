cc59becbc726109ca89266edaa7d99c11861543b
Wong Da You <64577096+wongdayou@users.noreply.github.com>
2026-02-01 17:21:26 +0800

    Added end game credits

--- Assets/High Level Systems/SceneManager/GlobalSceneManager.cs
+++ Assets/High Level Systems/SceneManager/GlobalSceneManager.cs
@@ -6,6 +6,7 @@ public class GlobalSceneManager : MonoBehaviour
     public string sceneToLoad;
     public void LoadChosenScene(string sceneToLoad)
     {
+        print("Loading main menu");
         SceneManager.LoadScene(sceneToLoad);
     }
 
